8e00ea4dfd7c9a60901216984327384377f805ed
Shimaa Sherif Khedr <56906347+shimaakhedr@users.noreply.github.com>
2022-06-01 19:04:02 +0300

    Update Figure2A.enrichment score_PanCancer_thorsson complex heatmap.R

--- R scripts/Figure2A.enrichment score_PanCancer_thorsson complex heatmap.R
+++ R scripts/Figure2A.enrichment score_PanCancer_thorsson complex heatmap.R
@@ -1,13 +1,9 @@
 # Setup environment
 rm(list = ls())
-load("~/R.Config.Rdata")
-setwd(master.location)
-setwd(paste0(master.location,"/TBI-LAB - Project - Pediatric Pan Cancer TARGET"))
 
 # Install packages and load
-source(paste0(toolbox.path,"/R scripts/ipak.function.R"))
 required.packages <- c("corrplot", "stringr")
-ipak(required.packages)                                                                                                  # Install and load required packages
+library(required.packages)                                                                                                  # Install and load required packages
 
 # Set Parameters
 Cancer = "PanCancer"                                                                                                    # Specify download method (this information to be used when saving the file)
