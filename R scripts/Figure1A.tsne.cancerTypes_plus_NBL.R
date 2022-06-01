f2773f290b6538468909e9fdf27b9941de287d9b
Shimaa Sherif Khedr <56906347+shimaakhedr@users.noreply.github.com>
2022-06-01 15:34:36 +0300

    Update Figure1A.tsne.cancerTypes_plus_NBL.R

--- R scripts/Figure1A.tsne.cancerTypes_plus_NBL.R
+++ R scripts/Figure1A.tsne.cancerTypes_plus_NBL.R
@@ -2,14 +2,10 @@
 
 #Setup environment
 rm(list = ls())
-load("~/R.Config.Rdata")
-setwd(master.location)
-setwd(paste0(master.location,"/TBI-LAB - Project - Pediatric Pan Cancer TARGET"))
-# Install packages and load
-source(paste0(toolbox.path,"/R scripts/ipak.function.R"))
 
+# Install packages and load
 required.packages <- c("Rtsne", "ggplot2","dplyr")
-ipak(required.packages)  
+library(required.packages)  
 
 # Set parameters
 Type = "Normalized" # "Raw" or "Normalized"
